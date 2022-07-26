42a593d251215cc90eb9f834b7a11b3ffe52cfb0
Plezi Devops <plezidevops@gmail.com>
2022-07-25 21:49:45 -0400

    adding geocode in separate module

---
+++ utils/geocode.js
@@ -0,0 +1,26 @@
+const request = require('postman-request');
+
+const geocode = (address, callback) => {
+  const accessToken = 'pk.eyJ1IjoidGlwaXpvIiwiYSI6ImNsNXlnYmY0eTB0ZWozam1tdHpwYjVwZHUifQ.6YJVhN0gbzDJCZSnjZn0tQ#downloadJSON=true';
+  const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(address)}.json?access_token=${accessToken}`;
+
+  request({ url: url, json: true }, (error, response, body) => {
+    if (error) {
+      callback('Unable to connect to location server', undefined);
+    } else if (body.features.length === 0) {
+      callback('Cannot find location', undefined);
+    } else {
+      const [longitude, latitude] = [body.features[0].center[0], body.features[0].center[1]];
+      callback(undefined, {
+        longitude,
+        latitude,
+        placeName: body.features[0].place_name
+      });
+    }
+  });
+
+};
+
+
+
+module.exports = geocode;
